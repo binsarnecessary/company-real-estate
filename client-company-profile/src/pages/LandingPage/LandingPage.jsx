89457572ab0e11386518acbb65a42afb1e37bbdc
muhammadansori123 <pangeranjawa212@gmail.com>
2023-11-23 21:20:52 +0700

    uplod ansor

--- client-company-profile/src/pages/LandingPage/LandingPage.jsx
+++ client-company-profile/src/pages/LandingPage/LandingPage.jsx
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
+import {Row,Col, Container } from 'react-bootstrap'
+
+import logo from '../../../public/logo192.png'
 
 export default class LandingPage extends Component {
   render() {
     return (
-      <div>LandingPage</div>
+      <div className='homepage'>
+        <header className='w-100 min-vh-100 d-flex align-items-center header header-bg pt-lg-5'>
+          <Container>
+            <Row className='header-box d-flex align-items-center'>
+                <Col lg="6" >
+                <img
+                  src={logo}
+                  alt="Company Logo"
+                  className="mr-3 rounded-circle"
+                  style={{ maxWidth: '100px', maxHeight: '100px' }}
+                />
+                  <h1 className='mb-4'>PT. DORMA MADA PERKASA</h1>
+                  <p>"Menyediakan Solusi Terbaik: Pengadaan Barang, Konstruksi, Mekanikal,<br/> 
+                    dan Interior, Untuk Mewujudkan Proyek Impian Anda!"</p>
+                </Col>
+            </Row>
+          </Container>
+        </header>
+      </div>
     )
   }
 }
+
+
