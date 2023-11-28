272c5626fc8652dc6afaabc6814c54f41aea250a
necessary16 <binsar16febuary@gmail.com>
2023-11-28 13:47:37 +0700

    change template

--- client-company-profile/src/pages/GalleryPage/GalleryPage.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class GalleryPage extends Component {
-  render() {
-    return (
-      <div>GalleryPage</div>
-    )
-  }
-}
--- client-company-profile/src/pages/KontakKamiPage/KontakKamiPage.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class KontakKamiPage extends Component {
-  render() {
-    return (
-      <div>KontakKamiPage</div>
-    )
-  }
-}
